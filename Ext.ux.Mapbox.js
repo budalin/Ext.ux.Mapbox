0ad9e2b31caedfa095a06b01109ff986826210a1
Arvin Dacumos <arvindacumos@gmail.com>
2012-11-13 01:15:27 +0800

    Ext.ux.Mapbox
    This is a simple wrapper for Mapbox (http://mabox.com/)  for use with
    Sencha Touch 2 (http://www.sencha.com/products/touch/)

---
+++ Ext.ux.Mapbox.js
@@ -0,0 +1,74 @@
+Ext.define('Ext.Mapbox', { 
+	extend: 'Ext.Component',
+	requires: ['Ext.device.Geolocation'],
+	map: null,
+	config: {
+		map: null
+	},
+	
+	constructor: function() {
+		this.callParent(arguments);
+		this.element.setVisibilityMode(Ext.Element.OFFSETS);
+		this.on('painted', this.showMap, this);
+	},
+	
+	showMap: function() {
+		
+		if(this.map) {
+			return true;
+		} 
+		
+		var me = this.map;
+		
+		me = mapbox.map(this.element.dom).zoom(2);
+		me.addLayer(mapbox.layer().id('arvindacumos.map-y2pwhu6l'));
+		me.ui.attribution.add().content('<a href="http://mapbox.com/about/maps">Terms &amp; Feedback</a>');
+		me.ui.zoomer.add();
+		// me.ui.refresh.add();
+		
+		/* create an empty layer */
+		var marker = mapbox.markers.layer();
+		me.addLayer(marker);
+		
+		Ext.device.Geolocation.getCurrentPosition({
+		    success: function(position) {
+		        // console.log(position.coords.latitude);
+				// zoom and update position
+				me.zoom(20).center({ 
+					lat: position.coords.latitude, 
+					lon: position.coords.longitude 
+				});
+				
+				marker.add_feature( {
+					geometry: {
+						coordinates: [
+							position.coords.latitude,
+							position.coords.longitude
+						]
+					},
+					properties: {
+						'marker_color': '#000',
+						'marker_symbol': 'star-stroked',
+					}
+				});
+
+		    },
+		    failure: function() {
+		        console.log('something went wrong!');
+		    }
+		});
+		
+		// this.map = mapbox.map(this.element.dom).zoom(2).center({ lat: 0, lon: 0 });
+		// this.map.addLayer(mapbox.layer().id('arvindacumos.map-y2pwhu6l'));
+		// this.map.ui.attribution.add().content('<a href="http://mapbox.com/about/maps">Terms &amp; Feedback</a>');
+		
+	},
+	
+	onUpdate: function (map, e, options) {
+        this.setHtml((options || {}).data);
+    },
+
+    onDestroy: function () {
+        this.callParent();
+    }
+});
